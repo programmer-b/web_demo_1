39ed903520ba8c8702c35ea9c68b5c69b2844b31
programmer-b <dancan219w@gmail.com>
2023-02-11 13:23:28 +0300

    Initial commit

---
+++ src/main/java/com/demo/web_demo_1/WebDemo1Application.java
@@ -0,0 +1,13 @@
+package com.demo.web_demo_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/web_demo_1/WebDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.web_demo_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
